0982c03bab7d3416cac7f663cd8254454b4636fc
francosolari <fsolari@comcast.net>
2024-11-01 19:07:02 -0400

    Network mode

--- nba_predictions/settings.py
+++ nba_predictions/settings.py
@@ -28,10 +28,10 @@ if IS_DEVELOPMENT:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DATABASE_NAME'),
-            'USER': os.getenv('DATABASE_USER'),
-            'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-            'HOST': os.getenv('DATABASE_HOST', 'localhost'),
+            'NAME': os.getenv('DATABASE_NAME', 'mydb'),
+            'USER': os.getenv('DATABASE_USER', 'myuser'),
+            'PASSWORD': os.getenv('DATABASE_PASSWORD', 'mypassword'),
+            'HOST': os.getenv('DATABASE_HOST', '134.209.213.185'),
             'PORT': os.getenv('DATABASE_PORT', '5432'),
         }
     }
